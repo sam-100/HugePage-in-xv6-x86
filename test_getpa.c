7116c6a21f4bacc4e7a7f1e0ed0865104b0b61ec
sam-100 <tandalesamarth@gmail.com>
2024-11-14 14:55:12 +0530

    getva() system call added.

---
+++ test_getpa.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    int *num = (int *) malloc(4);
+    *num = 100;
+
+    printf(1, "num = %d: va = %p, pa = %p\n", *num, num, getpa(num));
+    return 0;
+}
