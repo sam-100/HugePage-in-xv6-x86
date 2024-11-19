c6893ab804205ff5f1c95457af504bcba1ffa168
sam-100 <tandalesamarth@gmail.com>
2024-11-19 20:54:30 +0530

    cleaned test_promote.c

--- test_promote.c
+++ test_promote.c
@@ -5,48 +5,32 @@
 #define PGSIZE 4*1024
 #define HUGEPGSIZE 1024*PGSIZE
 
-#define BUFFER_SIZE (3*HUGEPGSIZE)
+#define BUFFER_SIZE (5*HUGEPGSIZE)
 #define INT_SIZE 4
+
+void print(const int *arr, int size, int jump) {
+    for(int i=0; i<size; i += jump) 
+        printf(1, "%d, ", arr[i]);
+    printf(1, "\n");
+}
+
 int main(int argc, char *argv[])
 {
     int *num = (int *)malloc(BUFFER_SIZE);
     for(int i=0; i<BUFFER_SIZE/INT_SIZE; i++)
-        num[i] = i/PGSIZE;
+        num[i] = i%10;
     
-    printf(1, "Before page promotion: \n");
-    for(int i=0; i<BUFFER_SIZE/4; i += HUGEPGSIZE/4)
-        printf(1, "%d | ", num[i]);
-    printf(1, "\n");
-
-    // if(getpagesize(num))
-    //     printf(1, "Huge page = Enabled\n");
-    // else 
-    //     printf(1, "Huge page = Disabled\n");
+    printf(1, "Before page promotion: ");
+    print(num, BUFFER_SIZE/INT_SIZE, HUGEPGSIZE/INT_SIZE);
 
     if(promote(num, BUFFER_SIZE) != 0)
     {
         printf(1, "promote() syscall failed\n");
         return 0;
     }
-    
+
     printf(1, "\nAfter page promotion: \n");
-    for(int i=0; i<BUFFER_SIZE/4; i += HUGEPGSIZE/4)
-    {
-        if(getpagesize(num+i) == 0)
-            printf(1, "Huge page disabled.\n");
-        else
-            printf(1, "Huge page enabled.\n");
-        printf(1, "virtual address: %p\n", num+i);
-        printf(1, "physical address: %p\n", getpa(num+i) & ~0x3fffff);
-        printf(1, "%d\n", num[i]);
-    }
-    printf(1, "\n");
-
-
-    // if(getpagesize(num))
-    //     printf(1, "Huge page = Enabled\n");
-    // else 
-    //     printf(1, "Huge page = Disabled\n");
+    print(num, BUFFER_SIZE/INT_SIZE, HUGEPGSIZE/INT_SIZE);
 
     printf(1, "success!\n");
 
